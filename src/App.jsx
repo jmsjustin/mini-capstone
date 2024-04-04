791a1995a76c4b8b1ef27d81db661d0a9044a95b
Justin Smith <jmsjustin@gmail.com>
2024-04-03 22:32:05 -0400

    fix axios

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { axios } from "axios";
+import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "mini-capstone-i6kv.onrender.com/";
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://mini-capstone-api-87e3.onrender.com";
 
 function App() {
   const [flashMessage, setFlashMessage] = useState("");
